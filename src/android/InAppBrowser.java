e726835ec85e67efd1947acfabec56db6bc3e247
권재원 <springkjw@gmail.com>
2018-12-15 01:40:04 +0900

    빠진 메서드 추가

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -68,6 +68,7 @@ import org.apache.cordova.PluginResult;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.net.URISyntaxException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
@@ -134,6 +135,19 @@ public class InAppBrowser extends CordovaPlugin {
     private boolean showFooter = false;
     private String footerColor = "";
     private String[] allowedSchemes;
+
+    public final static String ISP = "ispmobile";                               //	ISP모바일 : ispmobile://TID=nictest00m01011606281506341724
+	public final static String BANKPAY = "kftc-bankpay";
+	
+	public final static String LOTTE_APPCARD = "lotteappcard"; 					//	롯데앱카드 : intent://lottecard/data?acctid=120160628150229605882165497397&apptid=964241&IOS_RETURN_APP=#Intent;scheme=lotteappcard;package=com.lcacApp;end
+	public final static String HYUNDAI_APPCARD = "hdcardappcardansimclick"; 	//		현대앱카드 : intent:hdcardappcardansimclick://appcard?acctid=201606281503270019917080296121#Intent;package=com.hyundaicard.appcard;end;
+	public final static String SAMSUNG_APPCARD = "mpocket.online.ansimclick"; 	//	삼성앱카드 : intent://xid=4752902#Intent;scheme=mpocket.online.ansimclick;package=kr.co.samsungcard.mpocket;end;
+	public final static String NH_APPCARD = "nhappcardansimclick"; 				//	NH 앱카드 : intent://appcard?ACCTID=201606281507175365309074630161&P1=1532151#Intent;scheme=nhappcardansimclick;package=nh.smart.mobilecard;end;
+	public final static String KB_APPCARD = "kb-acp"; 							//	KB 앱카드 : intent://pay?srCode=0613325&kb-acp://#Intent;scheme=kb-acp;package=com.kbcard.cxh.appcard;end;
+	public final static String MOBIPAY = "cloudpay"; 							//	하나(모비페이) : intent://?tid=2238606309025172#Intent;scheme=cloudpay;package=com.hanaskcard.paycla;end;
+	
+	public final static String PACKAGE_ISP = "kvp.jjy.MispAndroid320";
+	public final static String PACKAGE_BANKPAY = "com.kftc.bankpay.android";
 
     /**
      * Executes the request and returns PluginResult.
@@ -1129,7 +1143,7 @@ public class InAppBrowser extends CordovaPlugin {
                     return true;
                 } catch (URISyntaxException ex) {
                     return false;
-                } catch (ActivityNotFoundException e) {
+                } catch (android.content.ActivityNotFoundException e) {
                     if ( intent == null )	return false;
 
                     //설치되지 않은 앱에 대해 market이동 처리
@@ -1170,6 +1184,26 @@ public class InAppBrowser extends CordovaPlugin {
             return false;
         }
 
+        /**
+        * @param scheme
+        * @return 해당 scheme에 대해 처리를 직접 하는지 여부
+        * 
+        * 결제를 위한 3rd-party 앱이 아직 설치되어있지 않아 ActivityNotFoundException이 발생하는 경우 처리합니다.
+        * 여기서 handler되지않은 scheme에 대해서는 intent로부터 Package정보 추출이 가능하다면 다음에서 packageName으로 market이동합니다.  
+        * 
+        */
+        protected boolean handleNotFoundPaymentScheme(String scheme) {
+            //PG사에서 호출하는 url에 package정보가 없어 ActivityNotFoundException이 난 후 market 실행이 안되는 경우
+            if ( ISP.equalsIgnoreCase(scheme) ) {
+                cordova.getActivity().startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + PACKAGE_ISP)));
+                return true;
+            } else if ( BANKPAY.equalsIgnoreCase(scheme) ) {
+                cordova.getActivity().startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + PACKAGE_BANKPAY)));
+                return true;
+            }
+            
+            return false;
+	    }
 
         /*
          * onPageStarted fires the LOAD_START_EVENT
